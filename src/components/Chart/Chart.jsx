9795a901ddcc9197e2e7e48c567d88f342fc3b3e
Joseph Iglecias <igleciasjoseph@gmail.com>
2020-05-15 00:08:26 -0400

    Initial without updated ReadME nor comments

---
+++ src/components/Chart/Chart.jsx
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from "react";
+import { fetchDailyData } from "../../api";
+import { Line, Bar } from "react-chartjs-2";
+
+import styles from "./Chart.module.css";
+
+const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
+  const [dailyData, setDailyData] = useState([]);
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      setDailyData(await fetchDailyData());
+    };
+
+    fetchAPI();
+  }, []);
+
+  // Line Chart is used ONLY for global selection because there is a date object from the API
+  const lineChart = dailyData.length ? (
+    <Line
+      data={{
+        labels: dailyData.map(({ date }) => date),
+        datasets: [
+          {
+            data: dailyData.map(({ confirmed }) => confirmed),
+            label: "Infected",
+            borderColor: "#3333ff",
+            fill: true,
+          },
+          {
+            data: dailyData.map(({ deaths }) => deaths),
+            label: "Deaths",
+            borderColor: "red",
+            backgroundColor: "rgba(255, 0, 0, 0.5)",
+            fill: true,
+          },
+        ],
+      }}
+    />
+  ) : null;
+
+  // Unfortunately, API doesnt have dates for specific country hence why you can't use a Line Chart and have to use a Bar chart
+  const barChart = confirmed ? (
+    <Bar
+      data={{
+        labels: ["Infected", "Recovered", "Deaths"],
+        datasets: [
+          {
+            label: "People",
+            backgroundColor: [
+              "rgba(0, 0, 255, 0.5)",
+              "rgba(0, 255, 0, 0.5)",
+              "rgba(255, 0, 0, 0.5)",
+            ],
+            data: [confirmed.value, recovered.value, deaths.value],
+          },
+        ],
+      }}
+      options={{
+        legend: { display: false },
+        title: { display: true, text: `Current State in ${country}` },
+      }}
+    />
+  ) : null;
+
+  return (
+    // Simple ternary deciding whether to display bar or line chart bases on criteria
+    <div className={styles.container}>{country ? barChart : lineChart}</div>
+  );
+};
+
+export default Chart;
