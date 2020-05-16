f00915ffbe5b3ce560193f10adcfb0faf1cde05c
Joseph Iglecias <igleciasjoseph@gmail.com>
2020-05-16 15:45:45 -0400

    added build for github pages

---
+++ build/precache-manifest.4df93a550f95497710dae810567951e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2e23beb3eb8390d38a0918e8e39c6de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7734c5d53edfbcf4f595",
+    "url": "/static/css/main.f557d1c5.chunk.css"
+  },
+  {
+    "revision": "7a0b6617cf1a7afef80b",
+    "url": "/static/js/2.ef57a0ef.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.ef57a0ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7734c5d53edfbcf4f595",
+    "url": "/static/js/main.85df7ba3.chunk.js"
+  },
+  {
+    "revision": "b21e2db45bb5cd26f15a",
+    "url": "/static/js/runtime-main.dca7fc49.js"
+  }
+]);
